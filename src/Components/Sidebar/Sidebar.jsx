b3a83373235b7c12c7d1ebbd674a674f245b80d8
zahra-m-27 <zahramazhar78@gmail.com>
2025-05-18 20:12:46 +0330

    add routing between pages

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,14 +2,18 @@ import styles from "./Sidebar.module.css";
 import SpeechIcon from "../../Assets/images/SpeechIcon.png";
 import ArchiveIcon from "../../Assets/images/ArchiveIcon.png";
 import SidebarIcon from "../../Assets/images/SidebarIcon.png";
-import { useState } from "react";
 import SidebarOption from "./SidebarOptions/SidebarOptions";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
-  const [isArchive, setIsArchive] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isArchive = location.pathname === "/archive";
+
   return (
     <div className={styles.main}>
-      <div className={styles.title}>
+      <div className={styles.title} onClick={() => navigate("/")}>
         <img src={SidebarIcon} />
         <span>آوا</span>
       </div>
@@ -17,14 +21,14 @@ export default function Sidebar() {
         <SidebarOption
           text="تبدیل گفتار"
           icon={SpeechIcon}
-          onCLick={() => setIsArchive(false)}
-          isSelected={isArchive ? false : true}
+          onCLick={() => navigate("/")}
+          isSelected={!isArchive}
         />
         <SidebarOption
           text="آرشیو"
           icon={ArchiveIcon}
-          onCLick={() => setIsArchive(true)}
-          isSelected={isArchive ? true : false}
+          onCLick={() => navigate("/archive")}
+          isSelected={isArchive}
         />
       </div>
     </div>
