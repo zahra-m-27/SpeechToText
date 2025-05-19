a9f18bd54d288da9f76fd3390b5554b426e83cce
zahra-m-27 <zahramazhar78@gmail.com>
2025-05-19 18:23:47 +0330

    add archiveSlice to redux (for ArchiveList state)

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import languageReducer from "./languageSlice";
+import userReducer from "./userSlice";
+import archiveReducer from "./archiveSlice";
 
 export const store = configureStore({
   reducer: {
     language: languageReducer,
+    user: userReducer,
+    archive: archiveReducer,
   },
 });
