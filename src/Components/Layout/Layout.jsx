3782238f7d48be8a2f504964b7b696d8ad44831f
zahra-m-27 <zahramazhar78@gmail.com>
2025-05-19 18:22:59 +0330

    add userSlice to redux

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,14 +1,25 @@
+import { useSelector } from "react-redux";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import styles from "./Layout.module.css";
 
 export default function Layout({ children }) {
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
   return (
     <div className={styles.container}>
       <Sidebar />
       <div className={styles.mainContent}>
         <Header />
-        {children}
+        {isLoggedIn ? (
+          children
+        ) : (
+          <>
+            <p style={{ fontSize: "25px", margin: "20px" }}>
+              لطفا ابتدا وارد شوید!
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
